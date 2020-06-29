27e6691db79fdd8aca433f3efeb3ce963a617ce7
Paulo Santoro <progersantoro@gmail.com>
2020-06-29 20:27:34 -0300

    Primeiro Commit do jogo

---
+++ script.js
@@ -0,0 +1,81 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 18;       
+let snake = [];
+snake[0] = {
+    x: 26 * box,    
+    y: 20 * box      
+}
+let direction = "right"
+let food = {
+    x: Math.floor(Math.random() * 52 + 1) * box,
+    y: Math.floor(Math.random() * 41 + 1) * box
+}
+
+function criarBg() {
+    context.fillStyle = "black";
+    context.fillRect(0,0,55 * box, 44 * box);   //16 , 16
+    
+}
+
+function criarCobrinha() {
+    for (i=0; i < snake.length; i++) {
+        context.fillStyle = "gray";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener ('keydown', update);
+function update (event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+
+}
+
+function iniciarJogo(){
+    if (snake[0].x > 54 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 55 * box;
+    if (snake[0].y > 43 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 44 * box;
+
+    for (i=1; i < snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert(' **** GAME OVER ****');
+        }
+    }
+
+    criarBg();
+    criarCobrinha();
+    drawFood();
+    
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    if (snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else {
+        food.x = Math.floor(Math.random() * 52 + 1) * box;
+        food.y = Math.floor(Math.random() * 41 + 1) * box;
+    }
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+
+}
+let jogo = setInterval(iniciarJogo, 100);
+
